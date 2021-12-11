88d0f927f0d3bc9b50547391478e98496f78d246
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2021-12-11 14:26:24 -0800

    Improve quality of life
    - Allow defining multi-word phrases.
    - Output helpful error messages when things go wrong.

--- define.sh
+++ define.sh
@@ -1,20 +1,42 @@
 #!/usr/bin/env sh
 # Define words without leaving your terminal.
-set -eu
+set -u
 
 # Find an API key here:
 #     https://dictionaryapi.com
 API_KEY='Put your API key here!'
 
+red=$(tput setaf 1)
+bold=$(tput bold)
+reset=$(tput sgr0)
+
+die() {
+	printf "${red}error:${reset} $@\n" 1>&2
+	exit 1
+}
+
 define() {
-	word="$1"
-	curl                                                                                  \
-		--silent                                                                          \
-		--show-error                                                                      \
-		"https://dictionaryapi.com/api/v3/references/collegiate/json/${1}?key=${API_KEY}" \
-	| jq                                                                                  \
-		--raw-output                                                                      \
-		'.[0].shortdef | join("\n")'
+	# Encode the word so it can be used in a URL.
+	word=$(printf %s "$1" | jq -sRr @uri)
+
+	url="https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${API_KEY}"
+
+	response=$(curl --silent "$url")
+
+	if [ "$response" = "[]" ]; then
+		die 'definition not found'
+	elif [ -z "$response" ]; then
+		die 'something went wrong'
+	fi
+
+	definition=$(echo "$response" | jq -r '.[0].shortdef | join("\n")' 2>/dev/null)
+
+	if [ "$?" -eq 5 ]; then
+		die 'definition not found'
+	fi
+
+	printf "${bold}${word}${reset}:\n"
+	printf "${definition}\n"
 }
 
 for word in "$@"; do
